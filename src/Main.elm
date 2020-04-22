587bb5ea72487427cba6e5e20b0937204846086c
Snoozing <at075027@gmail.com>
2020-04-22 22:33:35 +0100

    Add a main menu & some dummy pages where the generators & game will go

--- src/Main.elm
+++ src/Main.elm
@@ -1,20 +1,63 @@
-module Main exposing (..)
+module Main exposing (main)
 
 import Browser
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Element exposing (..)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Events exposing (..)
+import Element.Font as Font
+import Element.Input as Input
+
 
 
 ---- MODEL ----
 
 
+type alias Option =
+    { page : Page
+    , description : String
+    }
+
+
+options : List Option
+options =
+    [ { page = DungeonGenerator, description = "Dungeon Generator" }
+    , { page = CharacterGenerator, description = "Character Generator" }
+    , { page = TheGame, description = "Dungeon of Doom - the game" }
+    ]
+
+
+type Page
+    = MenuPage
+    | DungeonGenerator
+    | CharacterGenerator
+    | TheGame
+
+
 type alias Model =
-    {}
+    { currPage : Page
+    }
 
 
-init : ( Model, Cmd Msg )
-init =
-    ( {}, Cmd.none )
+type alias Flags =
+    ()
+
+
+init : Flags -> ( Model, Cmd Msg )
+init _ =
+    ( { currPage = MenuPage
+      }
+    , Cmd.none
+    )
+
+
+
+---- SUBSCRITPTIONS ----
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
 
 
 
@@ -22,35 +65,125 @@ init =
 
 
 type Msg
-    = NoOp
+    = OptionSelected Page
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        OptionSelected page ->
+            ( optionUpdate model page, Cmd.none )
+
+
+optionUpdate : Model -> Page -> Model
+optionUpdate model selected =
+    { currPage = selected }
 
 
 
 ---- VIEW ----
 
 
-view : Model -> Html Msg
+view : Model -> Browser.Document Msg
 view model =
-    div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+    case model.currPage of
+        MenuPage ->
+            menuPage
+
+        DungeonGenerator ->
+            holdingPage "Dungeon Generator"
+
+        CharacterGenerator ->
+            holdingPage "Character Generator"
+
+        TheGame ->
+            holdingPage "The Game"
+
+
+menuPage : Browser.Document Msg
+menuPage =
+    { title = "Dungeon of Doom"
+    , body =
+        [ layout [ Background.color black ] <|
+            column [ width fill, paddingXY 0 100 ]
+                [ column [ centerX, Border.width 4, Border.color white ]
+                    [ el [ paddingXY 100 10, Font.color green, Font.size 40 ] <| text "Dungeon of Doom"
+                    , column [ width fill, Font.color white, paddingXY 40 10, Border.widthEach { top = 4, left = 0, right = 0, bottom = 0 }, Border.color white, spacing 5 ]
+                        (List.map buildList options)
+                    ]
+                ]
         ]
+    }
+
+
+holdingPage : String -> Browser.Document Msg
+holdingPage pageName =
+    { title = "Dungeon of Doom - " ++ pageName
+    , body =
+        [ layout [ Background.color black ] <|
+            column [ width fill, paddingXY 0 100 ]
+            [ el [ Font.size 40, Font.color white ] <|
+                text pageName
+            , backButton
+            ]
+        ]
+    }
+
+
+backButton : Element Msg
+backButton =
+    Input.button
+        [ Background.color blue
+        , Element.focused
+            [ Background.color blue ]
+        ]
+        { onPress = Just (OptionSelected MenuPage)
+        , label = text "Main Menu"
+        }
+
+
+buildList : Option -> Element Msg
+buildList option =
+    el
+        [ mouseOver [ Background.color green, Font.color blue ]
+        , Element.Events.onClick (OptionSelected option.page)
+        , padding 2
+        , Font.alignLeft
+        , width fill
+        ]
+    <|
+        text option.description
+
+
+blue : Color
+blue =
+    rgb255 0 0 255
+
+
+black : Color
+black =
+    rgb255 0 0 0
+
+
+green : Color
+green =
+    rgb255 0 255 0
+
+
+white : Color
+white =
+    rgb255 255 255 255
 
 
 
 ---- PROGRAM ----
 
 
-main : Program () Model Msg
+main : Program Flags Model Msg
 main =
-    Browser.element
+    Browser.document
         { view = view
-        , init = \_ -> init
+        , init = init
         , update = update
-        , subscriptions = always Sub.none
+        , subscriptions = subscriptions
         }
